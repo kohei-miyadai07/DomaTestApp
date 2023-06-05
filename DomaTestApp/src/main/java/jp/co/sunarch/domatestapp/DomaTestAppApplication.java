12c4f2c689007ac2d1c6a4ecb7209ef7dbb63f58
kohei-miyadai07 <km57rf93@jcom.zaq.ne.jp>
2023-06-06 00:07:14 +0900

    Initial Commit

---
+++ DomaTestApp/src/main/java/jp/co/sunarch/domatestapp/DomaTestAppApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sunarch.domatestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomaTestAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomaTestAppApplication.class, args);
+	}
+
+}
---
+++ DomaTestApp/src/test/java/jp/co/sunarch/domatestapp/DomaTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sunarch.domatestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomaTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
